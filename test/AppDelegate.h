b7009e8e7bc0155b00792319c4579931fc20d7ed
유근명 <gmyou@onnuridmc.com>
2018-03-30 15:18:41 +0900

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Jack You on 2018. 3. 30..
+//  Copyright © 2018년 Jack You. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Jack You on 2018. 3. 30..
+//  Copyright © 2018년 Jack You. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
